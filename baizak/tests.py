1f12e4626e6cd4784ab14f98f7897c337b7bbddd
eldosagai <105664519+eldosagai@users.noreply.github.com>
2022-12-03 21:57:31 +0600

    kek

--- baizak/tests.py
+++ baizak/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from models import Article
-import pandas as pd
-print(pd.DataFrame(Article.objects.all().values()))
+# import pandas as pd
+# print(pd.DataFrame(Article.objects.all().values()))
 
 # Create your tests here.
